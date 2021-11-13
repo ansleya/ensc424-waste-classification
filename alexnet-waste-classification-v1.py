20653bbd5d7308967d210596a0c701ebd022bbaf
ansleya <55563396+ansleya@users.noreply.github.com>
2021-11-13 15:44:39 -0800

    bug fixes for storing to excel file

--- alexnet-waste-classification-v1.py
+++ alexnet-waste-classification-v1.py
@@ -14,32 +14,36 @@ import os
 import zipfile
 
 import openpyxl
-
-loadDir = input("Resume training on previous model (press enter to start from scratch): ")
-loadDir = Path(loadDir)
 # https://www.tensorflow.org/tutorials/images/classification
+loadDir = input("Type in directory of previous model (enter nothing to start from scratch): ")
+loadDir = Path(loadDir)
+
 ## Loading the data as a data object
 # set values for preparing the dataset below
 inVal_Split = 0.2 # splits data into training set and validation set (eg. 0.2 val -> 20% of files are for validation)
 inSeed = 123      # sets the seed so that the library remembers how they split up the above
-inImgSize = (227,227) 
+inImgSize = 227 
 saveDir = r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424'
+saveDir = Path(saveDir)
+if (not saveDir.is_dir()):
+  print("Error: invalid save directory")
+  exit()
 ############### need to sort data into directories - the function creates classes based on the number of folders
-train_ds = tf.keras.utils.image_dataset_from_directory(directory = r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424\garbage_classification',
+train_ds = tf.keras.utils.image_dataset_from_directory(directory = r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424\ansley-diego-dataset',
                                                       validation_split=inVal_Split,
                                                       seed = inSeed,
                                                       subset="training",
                                                       labels = "inferred",
                                                       batch_size = 128,
-                                                      image_size = inImgSize)
+                                                      image_size = (inImgSize,inImgSize))
 
-val_ds = tf.keras.utils.image_dataset_from_directory(directory = r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424\garbage_classification',
+val_ds = tf.keras.utils.image_dataset_from_directory(directory = r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424\ansley-diego-dataset',
                                                       validation_split=inVal_Split,
                                                       seed = inSeed,
                                                       subset="validation",
                                                       labels = "inferred",
                                                       batch_size = 128,
-                                                      image_size = inImgSize)
+                                                      image_size = (inImgSize,inImgSize))
 
 ##### for speeding up the runtime
 
@@ -48,7 +52,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 global model
-if (not loadDir.is_file()): 
+if (not loadDir.is_dir()): 
   ####### the CNN
   model = models.Sequential() 
   model.add(layers.Rescaling(1./255, input_shape=(227, 227, 3)))
@@ -82,7 +86,7 @@ if (not loadDir.is_file()):
   model.add(layers.Dense(8, activation='softmax'))
   model.summary()
 else:
-  model = models.load_model(r'C:\Users\ansle\OneDrive\Documents\SFU\Fourth Year\Fall 2021\ENSC 424\wastec.h5',
+  model = models.load_model(loadDir,
                             compile = False
   )
 
@@ -90,7 +94,7 @@ else:
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-epochs = 15
+epochs = 100
 callback = tf.keras.callbacks.EarlyStopping( # stop the run if the loss starts increasing
   monitor='val_loss',
   patience = 3,
@@ -102,8 +106,6 @@ history = model.fit(
   epochs=epochs,
   callbacks = [callback]
 )
-#history = model.fit(train_generator, epochs=25, steps_per_epoch=20, validation_data = validation_generator, verbose = 1, validation_steps=3)
-
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
@@ -117,33 +119,33 @@ plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
 plt.title('Training and validation accuracy')
 plt.legend(loc=0)
 plt.figure()
-
+plt.show()
 model.save(saveDir)
 
 wb = openpyxl.load_workbook('ensc424_waste_classification_testcases.xlsx')
 sheet = wb.get_sheet_by_name('AlexNet')
 
-i = 0
-while sheet['A' + str(i + 3)].value != None:
+i = 3
+while sheet['A' + str(i)].value != None:
   i += 1
 
 
 largest_val_acc = max(val_acc)
 optIndex  = val_acc.index(largest_val_acc)
-sheet['B' + i].value = "CPU"            #B - GPU or CPU
-sheet['C' + i].value = inVal_Split      #C - Validation Split
-sheet['D' + i].value = inSeed           #D - Dataset Seed
-sheet['E' + i].value = inImgSize        #E - Image Size
-sheet['F' + i].value = "yes"            #F - Standarization
-sheet['G' + i].value = optIndex             #G - Num Epochs (Best Performance)
-sheet['H' + i].value = acc[optIndex]              #H - Training Accuracy
-sheet['I' + i].value = largest_val_acc          #I - Validation Accuracy
-sheet['J' + i].value = loss[optIndex]           #J - Training Loss
-sheet['K' + i].value = val_loss[optIndex]        #K - Validation Loss
-sheet['L' + i].value = None    #L - Model File Name
+sheet['B' + str(i)].value = "CPU"            #B - GPU or CPU
+sheet['C' + str(i)].value = inVal_Split      #C - Validation Split
+sheet['D' + str(i)].value = inSeed           #D - Dataset Seed
+sheet['E' + str(i)].value = inImgSize        #E - Image Size
+sheet['F' + str(i)].value = "yes"            #F - Standarization
+sheet['G' + str(i)].value = optIndex + 1             #G - Num Epochs (Best Performance)
+sheet['H' + str(i)].value = acc[optIndex]              #H - Training Accuracy
+sheet['I' + str(i)].value = largest_val_acc          #I - Validation Accuracy
+sheet['J' + str(i)].value = loss[optIndex]           #J - Training Loss
+sheet['K' + str(i)].value = val_loss[optIndex]        #K - Validation Loss
+sheet['L' + str(i)].value = None    #L - Model File Name
 
 
 
 
 wb.save('ensc424_waste_classification_testcases.xlsx')
-plt.show()
+
